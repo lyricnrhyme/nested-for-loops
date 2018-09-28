12eaabf8e6c7513dc507dc051951ce7774f0b02f
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro-2.local>
2018-09-27 14:29:39 -1000

    da wae

--- js/nested-for-loops.js
+++ js/nested-for-loops.js
@@ -6,10 +6,29 @@
  */
 function nestedForLoops (depth, width = depth) {
   let result = "\n";
+  let newStr = '\n';
+  console.log('wut even', depth, width);
+  for (let i=0; i<depth; i++) {
+    for (let j=0; j<width; j++) {
+      newStr += `{x:${j}, y:${i}}, `
+    }
+    let noComma = newStr.split('');
+    noComma.pop();
+    noComma.pop();
+    newStr = noComma.join('');
+    // console.log('split?', noComma);
+    console.log('comma?', newStr)
+    newStr += result;
 
+  }
+  return newStr;
   // Write code here
 
 };
 
 // To see your console output outside the tests add function calls here.
 // console.log(nestedForLoops(4));
+
+//'\n{x:0, y:0}, {x:1, y:0}, {x:2, y:0}, {x:3, y:0}, {x:4, y:0}, {x:5, y:0}, {x:6, y:0}, {x:7, y:0}, {x:8, y:0}, {x:9, y:0}\n{x:0, y:1}, {x:1, y:1}, {x:2, y:1}, {x:3, y:1}, {x:4, y:1}, {x:5, y:1}, {x:6, y:1}, {x:7, y:1}, {x:8, y:1}, {x:9, y:1}\n{x:0, y:2}, {x:1, y:2}, {x:2, y:2}, {x:3, y:2}, {x:4, y:2}, {x:5, y:2}, {x:6, y:2}, {x:7, y:2}, {x:8, y:2}, {x:9, y:2}\n{x:0, y:3}, {x:1, y:3}, {x:2, y:3}, {x:3, y:3}, {x:4, y:3}, {x:5, y:3}, {x:6, y:3}, {x:7, y:3}, {x:8, y:3}, {x:9, y:3}\n{x:0, y:4}, {x:1, y:4}, {x:2, y:4}, {x:3, y:4}, {x:4, y:4}, {x:5, y:4}, {x:6, y:4}, {x:7, y:4}, {x:8, y:4}, {x:9, y:4}\n'
+
+//'\n{x:0, y:0}, {x:1, y:0}, {x:2, y:0}, {x:3, y:0}, {x:4, y:0}\n{x:0, y:1}, {x:1, y:1}, {x:2, y:1}, {x:3, y:1}, {x:4, y:1}\n{x:0, y:2}, {x:1, y:2}, {x:2, y:2}, {x:3, y:2}, {x:4, y:2}\n{x:0, y:3}, {x:1, y:3}, {x:2, y:3}, {x:3, y:3}, {x:4, y:3}\n{x:0, y:4}, {x:1, y:4}, {x:2, y:4}, {x:3, y:4}, {x:4, y:4}\n{x:0, y:5}, {x:1, y:5}, {x:2, y:5}, {x:3, y:5}, {x:4, y:5}\n{x:0, y:6}, {x:1, y:6}, {x:2, y:6}, {x:3, y:6}, {x:4, y:6}\n{x:0, y:7}, {x:1, y:7}, {x:2, y:7}, {x:3, y:7}, {x:4, y:7}\n{x:0, y:8}, {x:1, y:8}, {x:2, y:8}, {x:3, y:8}, {x:4, y:8}\n{x:0, y:9}, {x:1, y:9}, {x:2, y:9}, {x:3, y:9}, {x:4, y:9}\n'
